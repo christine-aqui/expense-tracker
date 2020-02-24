c87bfcfac87cccf68540995217ea2ed8a898309a
christine-aqui <aqui.christine@gmail.com>
2020-02-23 20:31:23 -0500

    exploring hooks and context

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<h2>
+			Expense Tracker!
+			<span role='img' aria-label='money with wings'>
+				💸
+			</span>
+		</h2>
+	);
+};
